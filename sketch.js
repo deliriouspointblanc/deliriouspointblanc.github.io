9e1f2d42cd6c2c28ca19eb974bd851b17f6ed37b
deliriouspointblanc <yasminmorgan.info@gmail.com>
2022-02-15 12:19:48 +0000

    added Prudence AR test

--- sketch.js
+++ sketch.js
@@ -1,38 +1,25 @@
-// Copyright (c) 2019 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
 /* ===
-ml5 Example
-LSTM Generator example with p5.js
-This uses a pre-trained model on a corpus of Virginia Woolf
+Based off ml5 Example: LSTM Generator example with p5.js
 For more models see: https://github.com/ml5js/ml5-data-and-training/tree/master/models/charRNN
-
-Let's create 2 charRNN instances, one for TORAII and one for HELICOID.
-
-//DONE: Make dialogue continuous/recursive
-- Add p5 shader with the webgl shaders
-- Experiment with more shader
-
-//Referencing Ellie's project (https://github.com/ellennickles/personalized-privacy-policy) on generating text using textarea and a smart way of updating the UI so that I don't just put the generated text into a p element
-
-Ellie combines text generation and stateful to allow char RNN to generate full sentences, checking in the generate function that if sentence begins with a period, autoGenerating stops.
-
-To-DO:
-- Grab the last sentence of what the model said & use it as input to the second model
-- After some time, delete the text and start fresh for more generating, or make it possible to scroll
-- Make it possible for users to write their own seed text and for the generation to restart.
-- move the nose with poseNet, changes z direction of shaders
+Copyright (c) 2019 ml5
+This software is released under the MIT License.
+https://opensource.org/licenses/MIT
 === */
 
-let toraii;
+/* === AMORPHOUS BUTTERFLY - Year 3 Final Project by Yasmin Morgan
+--> Speculation on machine relationships, what constitutes as intimacy?
+--> How can you be touched by this encounter, whatever it may be?
+--> based off of ml5.js tutorials and work done in 1st term Machine Learning for Creative Practice - charRNN and poseNet
+--> I learned the technique of using the textarea and stateful generation character by character from Ellen Nickles's project 'Personalized Privacy Policy' (https://github.com/ellennickles/personalized-privacy-policy). I reference her code using 'Nickle's code'.
+=== */
+
+let toraii; //two vars for lstm models
 let helicoid;
 
-let helicoidText;
+let helicoidText; //texture for shader
 let toraiiText;
 
-let textInput;
+let textInput; //DOM elements
 let textInput2;
 let lengthSlider;
 let lengthSlider2;
@@ -40,22 +27,10 @@ let tempSlider;
 let tempSlider2;
 let button;
 let button2;
-let runningInference = false;
-let runningInference2 = false;
-let model1;
-let model2;
-
+let runningInference = false; //switch to true for debugging     
+let runningInference2 = false; //without text generation
 let helicoidShader;
 let toraiiShader;
-
-//Ellie's code/ideas
-let currentText = '';
-let currentText_2 = '';
-let autoGenerating = false;
-let autoGenerating2 = false;
-let modelIsReady = false;
-
-let count = 0;
 let resetButton;
 
 //poseNet variables
@@ -63,50 +38,21 @@ let video;
 let poseNet;
 let pose;
 let skeleton;
-
 let brain;
 let poseLabel = "";
-
 let state = 'waiting';
 //let targetLabel;
 let targetRot;
-
 let xPosSlider, yPosSlider;
 let xPos; 
 let yPos; 
 
-function delay(time) { //from CodingTrain
-  return new Promise((resolve, reject) => {
-    if (isNaN(time)) {
-      reject(new Error('delay requires a valid number.'));
-    } else {
-      setTimeout(resolve, time);
-    }
-  });
-}
-
-async function keyPressed() {
-//  if (key == 't') {
-//    brain.normalizeData();
-//    brain.train({epochs: 50}, finished); 
-//  } else if (key == 's') {
-//    brain.saveData();
-//  } else if (key == 'd') {
-////    targetLabel = key;
-//    xPos = mouseX;
-//    yPos = mouseY;
-//    targetRot = [xPos, yPos];
-//    console.log(xPos, yPos);
-//    
-//    await delay(3000);
-//    console.log('collecting');
-//    state = 'collecting';
-//    
-//    await delay(5000);
-//    console.log('not collecting');
-//    state = 'waiting';
-//  }
-}
+//From Nickle's code
+let currentText = '';
+let currentText_2 = '';
+let autoGenerating = false;
+let autoGenerating2 = false;
+let modelIsReady = false;
 
 function preload() {
   toraiiShader = loadShader('assets/toraii.vert', 'assets/toraii.frag');
@@ -114,23 +60,18 @@ function preload() {
 }
 
 function setup() {
-  //noCanvas();
-  frameRate(12);
-  
-  if (!runningInference) {
-    setTimeout(onGenerateButton, 2000); //begin model 1 generation
-  }
+  frameRate(12); //reduce frameRate so project runs faster
  
   let canvas = createCanvas(windowWidth, windowHeight, WEBGL);
 
-  // Move the canvas so it's inside our <div id="sketch-holder">.
+  // Move canvas so inside the <div id="sketch-holder">.
   canvas.parent('sketch-holder');
   
   // Create the LSTM Generator passing it the model directory
   toraii = ml5.charRNN('/models/koansAndpoems/', modelReady);
-  helicoid = ml5.charRNN('/models/hemingway/', modelReady);
+  helicoid = ml5.charRNN('/models/murakami/', modelReady);
 
-   // initialize the createGraphics layers
+  // initialize the createGraphics layers
   helicoidText = createGraphics(800, 400, WEBGL);
   toraiiText = createGraphics(800, 400, WEBGL);
 
@@ -150,7 +91,11 @@ function setup() {
   
   select('#resetBoth').mousePressed(onResetBoth);
   
-  //poseNet parameters
+  if (!runningInference) {
+    setTimeout(onGenerateButton, 2000); //begin model 1 generation after 2 seconds of loading project
+  }
+  
+  //===== poseNet parameters =====//
   video = createCapture(VIDEO);
   video.hide();
   poseNet = ml5.poseNet(video, modelLoaded);
@@ -173,7 +118,7 @@ function setup() {
    };
    brain.load(modelInfo, brainLoaded);
 
-  // LOAD TRAINING DATA
+  // LOAD TRAINING DATA - only uncomment if training poseNet
 //   brain.loadData('targetRot2_poses.json', dataReady);
   
   xPosSlider = createSlider(0, 1000, 255);
@@ -183,7 +128,7 @@ function setup() {
   
 }
 
-// ELLIE ========
+// Nickle's code ========
 //: Read and seed with full text from input box
 function generateWithFullInputText() {
   let original = textInput.value();
@@ -208,12 +153,12 @@ function generateWithSingleChar_2() {
 
 // Update UI with current text
 function updateTextUI() {
-  select('#result').html(currentText); //this is how I get over the issue of using the p element, I just put everything into a textarea element.
+  select('#result').html(currentText); //this is how I get over the issue of generating text inside the p element. Instead, I put everything into a textarea element.
   checkTextareaHeight();
 }
 
 function updateTextUI_2() {
-  select('#result2').html(currentText_2); //this is how I get over the issue of using the p element, I just put everything into a textarea element.
+  select('#result2').html(currentText_2); //same for 2nd model
   checkTextareaHeight2();
 }
 
@@ -231,14 +176,13 @@ function onReset2Button() {
 }
 
 function onResetBoth() {
-  currentText = '';
+  currentText = ''; //textareas reset to empty
   updateTextUI();
   currentText_2 = '';
   updateTextUI_2();
   
   if (!runningInference) {
     setTimeout(onGenerateButton, 2000); //begin model 1 generation
-//    setInterval(onGenerateButton2, 10000);
   }
   
   autoGenerating = false;
@@ -258,9 +202,9 @@ function onGenerateButton2() {
   autoGenerating2 = true;
 }
 
-//=====ELLIE finished ====//
+//=====Nickle's functions finished ====//
 
-// Update the slider values
+// Update the slider values, template from ml5.js example.
 function updateSliders() {
 //  select('#length').html(lengthSlider.value());
   select('#temperature').html(tempSlider.value());
@@ -272,7 +216,6 @@ function updateSliders2() {
 }
 
 function modelReady() {
-  //select('#status').html('Model Loaded');
   console.log('Model Loaded');
   modelIsReady = true;
 }
@@ -346,94 +289,55 @@ function modelLoaded() {
   console.log('poseNet ready');
 }
 
-function draw() {
-  console.log(count);
-  
-  //poseNet stuff ========
-    let r = xPosSlider.value();
+function draw() {  
+  // ====== poseNet code ========== //
+  let r = xPosSlider.value();
   let g = yPosSlider.value();
-//  let b = bSlider.value();
   
   push();
   translate(width-video.width,-200,0);
   scale(-1, 1);
-  image(video, 0, 0, video.width, video.height);
+  /* === Webcam is not shown in this project but you can uncomment below for debugging purposes=== */
+  //poseNet might work better if video was positioned in the middle of project. Currently right behind the right-hand shader.
   
-  //Text to show where mouseX and mouseY is
-//  console.log("mouseX: " + mouseX);
-//  console.log("mouseY: " + mouseY);
-  
-//  push();
-//  noStroke();
-//  fill(r, g, b);
-//  rect(0, 0, 50, 50); //draws current slidervaue
-//  pop();
-  
-//  if (pose) {
-//    for (let i = 0; i < skeleton.length; i++) {
-//      let a = skeleton[i][0];
-//      let b = skeleton[i][1];
-//      strokeWeight(2);
-//      stroke(0);
-//
-//      line(a.position.x, a.position.y, b.position.x, b.position.y);
-//    }
-//    for (let i = 0; i < pose.keypoints.length; i++) {
-//      let x = pose.keypoints[i].position.x;
-//      let y = pose.keypoints[i].position.y;
-//      fill(0);
-//      stroke(255);
-//      ellipse(x, y, 16, 16);
-//    }
-//  }
+//  image(video, 0, 0, video.width, video.height);
   pop();
   
-  // ====== SHADER ========== //
-  // instead of just setting the active shader using shader() we are passing it to the createGraphics layer
+  // ====== SHADERS ========== //
+  // passing shaders into the createGraphics layer
   helicoidText.shader(helicoidShader);
   toraiiText.shader(toraiiShader);
   
-  // lets send the resolution, mouse, and time to our shader
-  // before sending mouse + time we modify the data so it's more easily usable by the shader
-  helicoidShader.setUniform('iResolution', [width/2, height]);
-  helicoidShader.setUniform('iMouse', [map(mouseX, 0, width, 0, 200), map(mouseY, 0, height, 0, 200)]);
+  // Send the resolution, mouse, and time to our shader
+  helicoidShader.setUniform('iResolution', [width*1.25, height*0.833]);
+  helicoidShader.setUniform('iMouse', [map(mouseX, 0, width, 0, 100), map(mouseY, 0, height, 0, 100)]);
+  helicoidShader.setUniform('poseScale', [map(r, .8, width, .5, 8.), map(g, 0., height, 0., 10.)]); //poseScale uniform changes the depth and thickness of helicoid shader, interesting effects
   helicoidShader.setUniform('iTime', frameCount * 0.05); 
   
   toraiiShader.setUniform('iResolution', [width, height]);
-  toraiiShader.setUniform('iMouse', [map(r, 0, width, 0, 100), map(g, 0, height, 0, 100)]);
+  toraiiShader.setUniform('iMouse', [map(mouseX, 0, width, 0, 200), map(mouseY, 0, height, 0, 200)]);
+  toraiiShader.setUniform('poseBox', [map(r, 1., width, 2., 8.), map(g, 0., height, 0., 10.)]); //poseBox increases/decreases the 'intensity' of the ribbon/ripple effect
   toraiiShader.setUniform('iTime', frameCount * 0.01);
   
   // passing the helicoidText layer geometry to render on
   helicoidText.rect(0,0,width/4 - 300,height);
   toraiiText.rect(0,0,width/4,height/2);
-
-  //background(0);
   
   // pass the shader as a texture
-  // anything drawn after this will have this texture.
   texture(helicoidText);
-  
-  
+
   push();
   translate(- (width/2) + 400,20, -100);
-//  rotateZ(theta * mouseX * 0.0001);
-//  rotateX(theta * mouseX * 0.0001);
-//  rotateY(theta * mouseX * 0.0001);  
-//  theta += 0.05;
   box(width/4 + 150, height/2 + 100);
   pop();
   
   texture(toraiiText);
   push();
   translate(width/2 - 400, 20, -100);
-//  rotateZ(theta * mouseX * 0.0001);
-//  rotateX(theta * mouseX * 0.0001);
-//  rotateY(theta * mouseX * 0.0001);  
-//  theta += 0.05;
   box(width/4 + 150, height/2 + 100);
   pop();
 
-  //Ellie
+  //Nickle's code - output the last letter of current text and seed into the next one, required to be in draw to generate.
   if(autoGenerating && modelIsReady) {
     generateWithSingleChar();
   } 
@@ -443,7 +347,7 @@ function draw() {
   }
 }
 
-//ELLIE====
+// ==== Nickles code==== //
 
 // Generate new text
 function generate(seed, stateful) {
@@ -451,8 +355,6 @@ function generate(seed, stateful) {
   if(!runningInference) {
     runningInference = true;
 
-    // Update the status log
-    //select('#status').html('Generating...');
     console.log('Generating...');
 
     let data = {
@@ -475,16 +377,12 @@ function generate(seed, stateful) {
         if (check) {
           // console.log("a period!");
           autoGenerating = false;
-          count += 1;
           setTimeout(onGenerateButton2, 5000); //when sentence finishes, triggers next model to start generating after 5 seconds
         }
 
         currentText += str; //adding result to current text
         updateTextUI();
       }
-      // Update the status log
-      status = 'Ready! '
-//      select('#status').html(status);
       runningInference = false;
     }
   }
@@ -494,9 +392,6 @@ function generate2(seed, stateful) {
     // prevent starting inference if we've already started another instance
   if(!runningInference2) {
     runningInference2 = true;
-
-    // Update the status log
-//    select('#status').html('Generating...');
 
     let data = {
       seed: seed,
@@ -518,20 +413,18 @@ function generate2(seed, stateful) {
         if (check) {
            console.log("a period!");
           autoGenerating2 = false;
-          count += 1;
           setTimeout(onGenerateButton, 5000);
         }
 
-        currentText_2 += str; //adding result to current text
+        currentText_2 += str; //incrementing result to current text
         updateTextUI_2();
       }
-      // Update the status log
-      status = 'Ready! '
-//      select('#status').html(status);
       runningInference2 = false;
     }
   }
 }
+
+// ==== Nickles code finished==== //
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
